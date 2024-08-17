b85004b376c6902af29862ba2aaac6e503c79604
Michael King <1747304+tsal@users.noreply.github.com>
2024-08-17 11:24:43 -0400

    fix: Bump to the next version

--- src/statics.rs
+++ src/statics.rs
@@ -1,4 +1,4 @@
 pub const COMMAND: &str = "archbelt";
-pub const VERSION: &str = "0.2.1";
+pub const VERSION: &str = "0.4.0";
 pub const DESCRIPTION: &str = "A tool to work with Archean blueprints and XenonCode modules";
 pub const ARCHEAN_STEAM_ID: u32 = 2941660;
